76af4fb0e061d79ff638d5441d07c749b971a5a2
Thomas <thomasmckenna12gmail.com>
2021-11-21 15:10:09 -0500

    react-hooks-react-app-example

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
 import TestComponent from "./TestComponent"
 // Add your code own within the return statement
+
+
 function App() {
   return (
     <div className="App">
       
-      <h1>{'Now'}</h1>
+      <h1>{"Now"}</h1>
       
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
@@ -16,6 +18,7 @@ function App() {
         self-contained chunks of JSX.
       </p>
       <ExampleComponent />
+    
       <TestComponent />
     </div>
   );
